8acfd2ec741f01478a5f768f11462b9dc58b3c34
dinhanit <ngodinhan2k3@gmail.com>
2023-10-07 15:35:01 +0700

    Edit ConfigModel

--- Model/ConfigModel.py
+++ Model/ConfigModel.py
@@ -4,19 +4,38 @@ import torch.nn as nn
 import torch
 import torch.optim as optim
 import json
-
+from torch.utils.data import DataLoader
+from torchvision import transforms
 import json
+import torchvision
 
-# Open and read the JSON file
 with open('Model/param.json', 'r') as json_file:
     data = json.load(json_file)
 
 for key, value in data.items():
     globals()[key] = value
+    
+train_transforms = transforms.Compose([
+    transforms.Resize((224)),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
-TRAINLOADER,TESTLOADER = None,None
+test_transforms = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
-model = CustomCNN()
+BATCH_SIZE = 64
+train_data = torchvision.datasets.ImageFolder(root='./Data/DataSets/SplitData/test', transform=train_transforms)
+test_data = torchvision.datasets.ImageFolder(root='./Data/DataSets/SplitData/train', transform=test_transforms)
+
+TRAINLOADER = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+TESTLOADER = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False)
+
+model = CustomCNN(num_classes=2)
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(DEVICE)
