0e33d14646312389cf9b4525178bd3434b1cb12a
huyhoanlee <hoanle2003@gmail.com>
2023-10-09 22:13:08 +0700

    edit .pkl

--- Data/main.py
+++ Data/main.py
@@ -1,18 +1,13 @@
 import pickle
 
-# def load_data(data): #FUNCTION TO LOAD DATA 
-#     with open(data, "rb") as file:
-#     # Load the serialized object from the file
-#         loaded_data = pickle.load(file)
-#     #print(loaded_data)
-
-#     print("Loaded Done")
-#     return loaded_data
-
-# load_data('train_data.pkl')
-# load_data('test_data.pkl')
-
-with open("train_data.pkl", "rb") as file:
+def load_data(data): #FUNCTION TO LOAD DATA 
+    with open(data, "rb") as file:
     # Load the serialized object from the file
-    loaded_data = pickle.load(file)
-print(loaded_data)
+        loaded_data = pickle.load(file)
+    print(loaded_data)
+
+    print("Loaded Done")
+    return loaded_data
+
+load_data('train_data.pkl')
+load_data('test_data.pkl')
