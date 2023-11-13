ba1e5a951ee8e6b012cd97cb6e2f61026a7804d2
hotonbao <htbqn2003@gmail.com>
2023-11-13 08:00:50 +0700

    modify batch size

--- Source/Model/param.py
+++ Source/Model/param.py
@@ -4,5 +4,5 @@ PATH_TRAIN = "../Data/train.npz"
 PATH_TEST = "../Data/test.npz"
 LEARNING_RATE= 0.0001
 EPOCHS=100
-BATCH_SIZE=32
+BATCH_SIZE=64
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
